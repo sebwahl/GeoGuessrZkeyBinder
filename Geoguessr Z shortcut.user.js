2e487ba14a032f70cad7795938a545fb1caf4601
Sebastien WAHL <sebwahl@users.noreply.github.com>
2023-09-09 13:53:00 +0200

    Update Geoguessr Z shortcut.user.js

--- Geoguessr Z shortcut.user.js
+++ Geoguessr Z shortcut.user.js
@@ -20,22 +20,48 @@ window.addEventListener('keydown', function(e) {
 });
 */
 
+var buttonMiddle = 2; // bouton de la molette
+var buttonRight = 3; // bouton droit
+var buttonFwd = 5; //bouton avance
+var buttonBack = 4; //bouton recule
+
+// Modification du bouton à prendre en compte
+var mappedButton = buttonMiddle;
+// Modification de la touche à prendre en compte : ligne
+var mappedKey_key ="z";
+var mappedKey_code = "Z";
+var mappedKey_keyCode = 0x5A;
+
+document.body.onauxclick = function(e) {
+    // on vérifie quel bouton de la souris a été cliqué
+    if (e.which == mappedButton) {
+        e.preventDefault(); //on ne fait pas ce que l'événement par défaut prévoit
+        retourArriere();
+    }
+    //alert("click " + e.which);
+    }
+
 document.body.onkeyup = function(e) {
     // on cherche si z ou Z est appuyé, et si on n'est pas sur un champ de saisie de texte
     var el = document.activeElement;
     var input_selected = (el && (el.tagName.toLowerCase() == 'input' && el.type == 'text' || el.tagName.toLowerCase() == 'textarea'))
 
-    if ((e.key == "z" ||
-        e.code == "Z" ||
-        e.keyCode == 0x5A)
+    if ((e.key == mappedKey_key ||
+        e.code == mappedKey_code ||
+        e.keyCode == mappedKey_keyCode)
         && !input_selected
        ) {
-        // clic sur le bouton de fonction "back"
-        var undo_move = $("[data-qa*='undo-move']");
-        if (undo_move.length){
-            // clic sur le premier trouvé de la liste (pourquoi y en aurait-il plusieurs ? allez savoir ...)
-            undo_move[0].click();
-            return
-        }
+        e.preventDefault(); //on ne fait pas ce que l'événement par défaut prévoit
+        retourArriere();
+    }
+}
+
+function retourArriere() {
+    // clic sur le bouton de fonction "back"
+    var undo_move = $("[data-qa*='undo-move']");
+    if (undo_move.length){
+        // clic sur le premier trouvé de la liste (pourquoi y en aurait-il plusieurs ? allez savoir ...)
+        undo_move[0].click();
+        return
     }
 }
